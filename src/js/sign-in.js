73fad01eed4a025d993438ef196675de05a315b0
kistoo <tomio.nakamura15@gmail.com>
2024-11-02 19:32:45 -0500

    Add form validation

---
+++ src/js/sign-in.js
@@ -0,0 +1,20 @@
+//validacion formulario
+const form = document.querySelector('form[name="frm"]');
+form.addEventListener("submit", (event) => {
+  const femail = form.elements["femail"].value;
+  const fpassword = form.elements["fpassword"].value;
+
+  if (!femail || !fpassword) {
+    event.preventDefault();
+    alert("Por favor, complete todos los campos del formulario");
+  } else if (!validateEmail(femail)) {
+    event.preventDefault();
+    alert("Por favor, ingrese un correo válido");
+  } 
+});
+
+//crear funcion validateEmail(femail)
+function validateEmail(femail) {
+  const re = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]/;
+  return re.test(String(femail).toLowerCase());
+}
